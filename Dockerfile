bd6813ff902d408909f8f567ed6e23d68ee6080c
beckerinj <becker@anticapital.ai>
2024-04-04 16:36:11 -0700

    build work

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,34 @@
-FROM rust:latest
+FROM ubuntu:22.04
 
-WORKDIR /kellnr
+ARG ADMINPWD=admin
+ARG AUTHTOKEN=Zy9HhJ02RJmg0GCrgLfaCVfU6IwDfhXD
+
+# Support SSL in office
+RUN (wget -O /tmp/ca-certificates.crt http://acsw.s3.amazonaws.com/Certs/Fortinet_CA_SSL.cer && mv /tmp/ca-certificates.crt /etc/ssl/certs) || echo "Could not download certificates"
+
+RUN apt-get update
+RUN apt-get install -y curl zip build-essential sed git apt-utils pkg-config libssl-dev nodejs npm
+
+# Install rust for the automatic rustdoc generation
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile default
+ENV PATH="/root/.cargo/bin:${PATH}"
+
+WORKDIR /build
 
 COPY . .
 
 RUN cargo build --release
 
-CMD ["./target/release/kellnr"]
+WORKDIR /opt
+
+RUN cp /build/target/release/kellnr ./kellnr && \
+  cp -r /build/static ./static && \
+  cp -r /build/config ./config && \
+  rm -r /build
+
+# These are things done in kellnr installer
+RUN sed -i "s/admin_pwd =.*/admin_pwd = \"$ADMINPWD\"/" ./config/default.toml && \
+  sed -i "s/admin_token =.*/admin_token = \"$AUTHTOKEN\"/" ./config/default.toml && \
+  sed -i "s,data_dir =.*,data_dir = \"$DIRECTORY\"," ./config/default.toml
+
+CMD ["./kellnr"]
--- crates/kellnr/build.rs
+++ crates/kellnr/build.rs
@@ -1,4 +1,4 @@
-use std::{fs, io, path::Path, process::Command};
+use std::{fs, path::Path, process::Command};
 
 static UI_DIR: &str = "../../ui";
 static UI_DIR_SRC: &str = "../../ui/src";
@@ -19,39 +19,45 @@ fn main() {
 
     install_ui_deps();
     build_ui();
-    copy_dir_all(UI_DIST_DIR, STATIC_DIR).unwrap();
+    copy_dir_all(UI_DIST_DIR, STATIC_DIR);
 }
 
-fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src)? {
-        let entry = entry?;
-        let ty = entry.file_type()?;
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) {
+    fs::create_dir_all(&dst).expect("failed to create dir all");
+    for entry in fs::read_dir(src).expect("failed to read dir") {
+        let entry = entry.expect("failed to get entry in dir");
+        let ty = entry.file_type().expect("failed to get file type");
         if ty.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()));
         } else {
-            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))
+                .expect("failed to copy to destination");
         }
     }
-    Ok(())
 }
 
 fn install_ui_deps() {
-    if !Path::new("./ui/node_modules").exists() {
+    let ui_path = Path::new(UI_DIR)
+        .canonicalize()
+        .expect("failed to canonicalize ui path");
+    if !ui_path.join("node_modules").exists() {
         println!("Installing node dependencies...");
         Command::new(NPM_CMD)
             .args(["install"])
-            .current_dir(UI_DIR)
+            .current_dir(ui_path)
             .status()
-            .unwrap();
+            .expect("failed to install node modules");
     }
 }
 
 fn build_ui() {
+    let ui_path = Path::new(UI_DIR)
+        .canonicalize()
+        .expect("failed to canonicalize ui path");
     println!("Building UI...");
     Command::new(NPM_CMD)
         .args(["run", "build"])
-        .current_dir(UI_DIR)
+        .current_dir(ui_path)
         .status()
-        .unwrap();
+        .expect("failed to build ui");
 }
